faa89387cc4bb92d7057fd5548108834cf84adc6
Karen Garcia dos Santos <karenkgs@gmail.com>
2018-05-13 20:25:23 -0300

    Adding route file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-import './assets/css/index.css';
-
 import { HashRouter, Route, Switch } from "react-router-dom";
 
 import App from '.././App';
