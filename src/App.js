ee9b994dd2bd8aa763102a54422c7b59a3c255b5
Karen Garcia <karenkgs@gmail.com>
2020-04-18 14:53:22 +1000

    Making it pretty

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="left">
+          <p><span>Karen</span></p>
+        </div>
+        <nav>
+          <div className="App-header-links">
+            <ul>
+              <li><a href="#App">About</a></li>
+              <li><a href="#App">Projects</a></li>
+              <li><a href="#App">Talks</a></li>
+              <li><a href="#App">Contact</a></li>
+            </ul>
+          </div>
+        </nav>
       </header>
+      <body>
+        <p>This is the body</p>
+      </body>
+      <footer>
+        Karen inc 2020
+      </footer>
     </div>
   );
 }
